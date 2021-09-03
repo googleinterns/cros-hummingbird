4879e5f9263a648e5071fd648f6ac3c769ec06a3
Peggy Lin <peggylin@google.com>
2021-09-03 10:47:28 +0800

    splitext, rm line

--- csv_convert/rigol.py
+++ csv_convert/rigol.py
@@ -16,17 +16,17 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   if args.output_csv is None:
-    args.output_csv = args.input_csv[:-4]+"_converted.csv"
+    name, ext = os.path.splitext(args.input_csv)
+    args.output_csv = name + "_converted" + ext
 
-  if os.path.isfile(args.input_csv):
-    with open(args.input_csv, "r") as f:
-      data_iter = csv.reader(f, delimiter=",")
-      channel_info = next(data_iter)[1:3]
-      time_info = next(data_iter)[-1]
-      with open(args.output_csv, "w", newline="") as f_out:
-        writer = csv.writer(f_out, delimiter=",")
-        writer.writerow(["Time [s]", channel_info[0], channel_info[1]])
-        for data in data_iter:
-          data = data[:3]
-          data[0] = int(data[0]) * float(time_info)
-          writer.writerow(data)
+  with open(args.input_csv, "r") as f:
+    data_iter = csv.reader(f, delimiter=",")
+    channel_info = next(data_iter)[1:3]
+    time_info = next(data_iter)[-1]
+    with open(args.output_csv, "w", newline="") as f_out:
+      writer = csv.writer(f_out, delimiter=",")
+      writer.writerow(["Time [s]", channel_info[0], channel_info[1]])
+      for data in data_iter:
+        data = data[:3]
+        data[0] = int(data[0]) * float(time_info)
+        writer.writerow(data)
