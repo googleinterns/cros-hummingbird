f977b588b9e73a3fca16ad0ed9501a6ed42c5c9c
Peggy Lin <peggylin@google.com>
2021-09-09 10:01:18 +0800

    t_sp

--- hummingbird.py
+++ hummingbird.py
@@ -406,6 +406,7 @@ class HummingBird():
     addr = ""
     scl_skip = 0
     sda_skip = 0
+    t_sp = 5e-8  # ignore spikes with pulse width < 50ns
     for i in range(1, len(self.sda_data)):
       n_sda = self.sda_data[i]
       n_scl = self.scl_data[i]
@@ -420,7 +421,7 @@ class HummingBird():
           )
           self.scl_falling_edge += 1
           scl.low_start = scl.i_30p
-          scl_skip = i + 5e-8 / self.sampling_period
+          scl_skip = i + t_sp / self.sampling_period
           scl.i_30p = scl.i_70p = None
 
           ## Don't take t_buf into T_clk consideration
@@ -478,7 +479,7 @@ class HummingBird():
           )
           self.scl_rising_edge += 1
           scl.high_start = scl.i_70p
-          scl_skip = i + 5e-8 / self.sampling_period
+          scl_skip = i + t_sp / self.sampling_period
           scl.i_30p = scl.i_70p = None
 
           ## Use data_start_flag avoid taking t_buf into T_clk
@@ -545,7 +546,7 @@ class HummingBird():
           )
           self.sda_falling_edge += 1
           sda.low_start = sda.i_30p
-          sda_skip = i + 5e-8 / self.sampling_period
+          sda_skip = i + t_sp / self.sampling_period
           sda.i_30p = sda.i_70p = None
         else:
           if (sda.i_30p - sda.low_end) * self.sampling_period > 1e-7:
@@ -581,7 +582,7 @@ class HummingBird():
           )
           self.sda_rising_edge += 1
           sda.high_start = sda.i_70p
-          sda_skip = i + 5e-8 / self.sampling_period
+          sda_skip = i + t_sp / self.sampling_period
           sda.i_30p = sda.i_70p = None
         else:
           if (sda.i_70p - sda.high_end) * self.sampling_period > 1e-7:
